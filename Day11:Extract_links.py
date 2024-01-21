21a3b7212e1ad935b6ba7562a3e86225aa5a396c
Kao Saelor <saelor.kao@gmail.com>
2024-01-21 07:12:17 -0800

    extract link

--- Day11:Extract_links.py
+++ Day11:Extract_links.py
@@ -1,3 +1,26 @@
 ### Day 11: Extract Links
 # Modify the script to extract and print all the links (href attributes) from the webpage.
 
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_html_content(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Use BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract and print all the links from webpage
+        links = soup.find_all('a', href=True)
+        for link in links:
+            print(f"Link: {link['href']}")
+
+    else:
+        print(f"Failed to fetch the HTML.")
+
+website_url = 'http://www.python.org/'
+fetch_html_content(website_url)
+
