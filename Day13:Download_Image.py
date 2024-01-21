470e264f3b43692f8d7fdb35d97f3ca7478edbd7
Kao Saelor <saelor.kao@gmail.com>
2024-01-21 10:28:04 -0800

    create function

--- Day13:Download_Image.py
+++ Day13:Download_Image.py
@@ -1,3 +1,16 @@
 ### Day 13: Download an Image
 
 # Use the `requests` library to download an image from the webpage to your local machine.
+
+import requests
+from bs4 import urljoin
+from urllib.parse import urljoin
+
+def fetch_html_content(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Use BeautifulSoup to parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
