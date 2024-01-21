505fb619900971d25cc4375f4567c5eefd655f7e
Kao Saelor <saelor.kao@gmail.com>
2024-01-20 21:40:24 -0800

    move to directory

--- Day07:Move_to_directory.py
+++ Day07:Move_to_directory.py
@@ -6,3 +6,11 @@ def create_directory(directory_name):
         # Create the new directory
         os.makedirs(directory_name)
         print(f"Directory {directory_name} created successfully.")
+    else:
+        print(f"Error: Directory {directory_name} already exists.")
+
+# Directory creation
+directory_name = "my_directory07"
+
+# Call the function to create the directory
+create_directory(directory_name)
