00b4f37542532f7c9cc476f745acc5313c710148
Kao Saelor <saelor.kao@gmail.com>
2024-01-21 01:39:37 -0800

    web scraping

--- Day09:Web_Scraping.py
+++ Day09:Web_Scraping.py
@@ -4,6 +4,16 @@
 
 import requests
 
-def_fetch_html_content(url):
-# Send a GET Request to the URL
-response = requests.get(url)
+def fetch_html_content(url):
+    # Send a GET Request to the URL
+    response = requests.get(url)
+
+    # Check if the requeest was successful
+    if response.status_code == 200:
+        # Print the HTML content of the website
+        print(response.text)
+    else:
+        print(f"Failed to fetch the HTML.  Status Code: {response.status_code}")
+
+website_url = 'https://www.python.org/'
+fetch_html_content(website_url)
