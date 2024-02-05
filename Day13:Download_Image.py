235849e30bf1bf669678d5305f4588faf0c28f25
Kao Saelor <saelor.kao@gmail.com>
2024-02-04 18:13:18 -0800

    work on download image

--- Day13:Download_Image.py
+++ Day13:Download_Image.py
@@ -14,3 +14,18 @@ def fetch_html_content(url):
     if response.status_code == 200:
         # Use BeautifulSoup to parse the HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract the first image source (src attribute) from the webpate
+        image = soup.find('img', src=True)
+        if image:
+            # Get the absolute URL of the image using urljoin
+            image_url = urljoin(url, image['src'])
+
+            # Download the image to the local machine
+            image_response = requests.get(image_url)
+
+            # Check if the image download succeeded
+            if image_response.status_code == 200:
+                # Save the image to a local file
+                with open('download_image.jpd', 'wb') as f:
+                    f.write(image_response.content)
