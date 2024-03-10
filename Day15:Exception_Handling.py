58aff1d160e18eddac5125d6584bd450223472a9
Kao Saelor <saelor.kao@gmail.com>
2024-03-09 16:34:13 -0800

    work on day 15 exception handling

--- Day15:Exception_Handling.py
+++ Day15:Exception_Handling.py
@@ -22,3 +22,25 @@ def read_file(file_path):
         return content
     
 def write_file(file_path, content):
+    file = None
+    if os.access(file_path, os.W_OK):
+        file = open(file_path, 'W')
+        file.write(content)
+        print(f"File {file_path} successfully written.")
+    else:
+        print(f"Error: Permission denied.  Unable to write to file {file_path}.")
+    if file is not None:
+        file.close()
+
+# Example usage
+file_path = "example.txt"
+
+# Read from File
+file_content = read_file(file_path)
+
+if file_content is not None:
+    # Modify content
+    modified_content = file_content + "\nNew line added."
+
+    # Write back to file
+    write_file(file_path, modified_content)
