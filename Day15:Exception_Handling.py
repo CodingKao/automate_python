0302b3ee9fbf6f8a48e37e91c0ca28c909cdb32a
Kao Saelor <saelor.kao@gmail.com>
2024-03-09 16:45:56 -0800

    added example.txt

--- Day15:Exception_Handling.py
+++ Day15:Exception_Handling.py
@@ -8,38 +8,37 @@ def read_file(file_path):
     if os.path.exists(file_path):
         file = None
         content = None
-
         if os.access(file_path, os.R_OK):
             file = open(file_path, 'r')
             content = file.read()
         else:
-            print(f"Error: Permission denied.  Unable to read from file {file_path}.")
+            print(f"Error: Permission denied. Unable to read from file {file_path}.")
         if file is not None:
             file.close()
         return content
     else:
         print(f"Error: File {file_path} not found.")
-        return content
-    
+        return None
+
 def write_file(file_path, content):
     file = None
     if os.access(file_path, os.W_OK):
-        file = open(file_path, 'W')
+        file = open(file_path, 'w')
         file.write(content)
         print(f"File {file_path} successfully written.")
     else:
-        print(f"Error: Permission denied.  Unable to write to file {file_path}.")
+        print(f"Error: Permission denied. Unable to write to file {file_path}.")
     if file is not None:
         file.close()
 
 # Example usage
 file_path = "example.txt"
 
-# Read from File
+# Read from file
 file_content = read_file(file_path)
 
 if file_content is not None:
-    # Modify content
+    # Modify content (for example, add a new line)
     modified_content = file_content + "\nNew line added."
 
     # Write back to file
