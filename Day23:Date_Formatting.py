64005bb698604b65aad11759fd1727f1faf744bd
Kao Saelor <saelor.kao@gmail.com>
2024-03-18 18:55:52 -0700

    format date and time day 23

---
+++ Day23:Date_Formatting.py
@@ -0,0 +1,16 @@
+### Day 23: Date Formatting
+
+## Modify the script to print the current date and time in a specific format.
+
+import datetime
+
+def current_date_time():
+    current_date_time = datetime.datetime.now()
+    print("Current date and time: ", current_date_time)
+
+    formatted_date_time = current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time: ", formatted_date_time)
+
+
+# Print Output
+current_date_time()
