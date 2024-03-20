4307067f79eef7f7333dab0b819bdf9d32d87034
Kao Saelor <saelor.kao@gmail.com>
2024-03-19 21:08:36 -0700

    start on day 24 os.path

--- Day24:File_Operation.py
+++ Day24:File_Operation.py
@@ -2,3 +2,18 @@
 
 ### Explore the `os.path` module and implement basic file operations like joining paths and checking file existence.
 
+import os
+
+# Join paths
+
+path1 = "folder1"
+path2 = "file.txt"
+joined_path = os.path.join(path1, path2)
+print("joined path:", joined_path)
+
+# Check file existence
+file_to_check = "file.txt"
+if os.path.exists(file_to_check):
+    print(f"File {file_to_check} exist.")
+else:
+    print(f"File {file_to_check} does not exist.:")
