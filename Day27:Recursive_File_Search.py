85890019aa23ccf5ae2f9c19a40025280f59cb49
Kao Saelor <saelor.kao@gmail.com>
2024-03-24 17:46:00 -0700

    work on day 27

--- Day27:Recursive_File_Search.py
+++ Day27:Recursive_File_Search.py
@@ -6,6 +6,9 @@ import os
 
 def search_files_with_extension(directory, extension, result=[]):
     if os.path.isdire(directory):
-        item_path = os.path.join(directory, item)
-        if os.path.isdire(item_path):
-            
+        for item in os.listdir(directory):
+            item_path = os.path.join(directory, item)
+            if os.path.isdir(item_path):
+                search_files_with_extension(item_path, extension, result)
+            elif os.path.isfile(item_path) and item.endswith(extension):
+                result.append(item_path)
