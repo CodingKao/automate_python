9b194c1afe6ee99035b7230f5b1c49b5ef3c1943
Kao Saelor <saelor.kao@gmail.com>
2024-04-01 20:49:13 -0700

    try GET request with response

--- Day29:JSON_API.py
+++ Day29:JSON_API.py
@@ -3,3 +3,16 @@
 ## Extend the script to interact with a JSON-based API, fetch data, and print relevant information.
 
 import requests
+
+def fetch_data():
+    # Make GET request to API
+    response = request.get("https://www.python.org/")
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        return data
+    else:
+        print("Failed to fetch data.  Status code:", response.status_code)
+        return None
