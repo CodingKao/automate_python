afe8ac2dc54376165e10f3693d659c179179898a
Kao Saelor <saelor.kao@gmail.com>
2024-04-04 18:19:55 -0700

    start on day 30

---
+++ Day30:Simple_Automation.py
@@ -0,0 +1,9 @@
+### Day 30: Capstone Project - Simple Automation Task
+
+### Apply the skills learned in the past 29 days to automate a simple everyday task, such as organizing files, downloading data, or sending emails.
+
+#Objective:
+### Automate the organization of files by categorizing them into subdirectories based on their file extensions.
+
+import os
+import shutil
